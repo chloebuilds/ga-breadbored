3e44fc2139ab8a3dbeb5b85218ebce26dacfbcfe
alara <alaraayan@hotmail.com>
2022-04-25 21:22:22 +0100

    added comments to the bread model

--- models/bread.js
+++ models/bread.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose'
 import mongooseUniqueValidator from 'mongoose-unique-validator'
 
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true, maxlength: 200 },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+},{
+  timestamps: true,
+}
+)
+
 const breadSchema = new mongoose.Schema({
   name: { type: String, required: true },
   origin: { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
+  comments: [commentSchema],
   addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
 })
 
